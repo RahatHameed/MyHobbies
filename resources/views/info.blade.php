997ed9e70f52f2731d30906c140d69937b611475
Rahat <rahat.hameed@zigron.com>
2020-08-30 20:16:54 +0500

    Adding Page titles

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+@section('page_title','About My hobbies')
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
--- resources/views/starting_page.blade.php
+++ resources/views/starting_page.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+@section('page_title','Starting page')
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
