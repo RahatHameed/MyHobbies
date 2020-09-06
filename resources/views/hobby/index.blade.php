4c540b77341ebf2993a13742f06a16c2db3c7416
Rahat <rahat.hameed@zigron.com>
2020-09-06 16:55:28 +0500

    Delete records with destroy()

--- resources/views/hobby/index.blade.php
+++ resources/views/hobby/index.blade.php
@@ -10,9 +10,16 @@
                     <div class="card-body">
                         <ul class="list-group">
                             @foreach($hobbies as $hobby)
-                                <a title="Show Details" href="/hobby/{{ $hobby->id }}">{{ $hobby->name }}</a>
-                                <a class="btn btn-sm btn-light ml-2" href="/hobby/{{ $hobby->id }}/edit"><i class="fas fa-edit"></i> Edit Hobby</a>
-                                @endforeach
+                                <li class="list-group-item">
+                                    <a title="Show Details" href="/hobby/{{ $hobby->id }}">{{ $hobby->name }}</a>
+                                    <a class="btn btn-sm btn-light ml-2" href="/hobby/{{ $hobby->id }}/edit"><i class="fas fa-edit"></i> Edit Hobby</a>
+                                    <form class="float-right" style="display: inline" action="/hobby/{{ $hobby->id }}" method="post">
+                                        @csrf
+                                        @method("DELETE")
+                                        <input class="btn btn-sm btn-outline-danger" type="submit" value="Delete">
+                                    </form>
+                                </li>
+                            @endforeach
                         </ul>
                     </div>
 
