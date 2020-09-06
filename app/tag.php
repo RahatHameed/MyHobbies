356901889562f578272a254cd9ee1b3129e5d8bf
Rahat <rahat.hameed@zigron.com>
2020-09-06 22:33:31 +0500

    Link models with Eloquent Relationships

--- app/tag.php
+++ app/tag.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class tag extends Model
 {
+
+    public function hobbies() {
+        return $this->belongsToMany('App\Hobby');
+    }
+
     protected $fillable = [
         'name','style',
     ];
