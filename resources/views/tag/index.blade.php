3dc04be541fde7c4986e4dafbcb814aff8554386
Rahat <rahat.hameed@zigron.com>
2020-09-10 04:12:18 +0500

    Control buttons in the frontend with @can

--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -14,17 +14,25 @@
                                 <li class="list-group-item">
                                     <span style="font-size: 130%;" class="mr-2 badge badge-{{ $tag->style }}">{{ $tag->name }}</span>
 
+                                    @can('update', $tag)
                                     <a class="ml-2 btn btn-sm btn-outline-primary" href="/tag/{{ $tag->id }}/edit"><i class="fas fa-edit"></i> Edit</a>
+                                    @endcan
+
+                                    @can('delete', $tag)
                                     <form style="display: inline;" action="/tag/{{ $tag->id }}" method="post">
                                         @csrf
                                         @method('DELETE')
                                         <input class="btn btn-outline-danger btn-sm ml-2" type="submit" value="Delete">
                                     </form>
+                                    @endcan
                                     <span class="float-right"><a href="{{route('FilterHobbiesByTagID', ['tag_id' => $tag->id])}}"> Used {{$tag->hobbies()->count()}} times</a></span>
                                 </li>
                             @endforeach
                         </ul>
+
+                        @can('update', $tag)
                         <a class="btn btn-success btn-sm mt-3" href="/tag/create"><i class="fas fa-plus-circle"></i> New Tag</a>
+                        @endcan
                     </div>
                 </div>
             </div>
